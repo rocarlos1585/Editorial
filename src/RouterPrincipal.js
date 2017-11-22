4f930ebed38d0de58e38d5b94a3b88e4d38cf659
Roberto Lopez <roberto.karlos.lopez@gmail.com>
2017-11-21 20:53:29 -0600

    se agregaron routers privados faltan los routers escuela y editorial

--- src/RouterPrincipal.js
+++ src/RouterPrincipal.js
@@ -1,25 +1,77 @@
 import React,{Component} from 'react'
 import {Route, BrowserRouter, Link, Redirect, Switch,Router} from 'react-router-dom'
 import * as firebase from 'firebase'
+import Login from './Loginform.js'
+import Escuela from './Escuela.js'
 
-function PrivateRoute ({component: Component, authed, ...rest}) {
+function PrivateRouteEditorial ({component: Component, authed,user, ...rest}) {
   return (
     <Route
       {...rest}
-      render={(props) => authed === true
+      render={(props) => authed === true &&  user === 'adan1995a@gmail.com'
         ? <Component {...props} />
-        : <Redirect to={{pathname: '/login' , state: {from: props.location}}} />}
+        : <Redirect to={{pathname: '/editorial' , state: {from: props.location}}} />}
+    />
+  )
+}
+function PrivateRouteEscuela ({component: Component, authed,user, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true &&  user !== 'adan1995a@gmail.com'
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/escuela' , state: {from: props.location}}} />}
     />
   )
 }
 
-function PublicRoute ({component: Component, authed, ...rest}) {
+function PublicRouteLogin ({component: Component, authed, ...rest}) {
   return (
     <Route
       {...rest}
       render={(props) => authed === false
         ? <Component {...props} />
-        : <Redirect to='/Admin' />}
+        : <Redirect to='/login' />}
     />
   )
+}
+
+class Routes extends Component {
+  state = {
+    authed: false,
+    loading: true,
+  }
+  componentDidMount () {
+
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          loading: false,
+        })
+      } else {
+        this.setState({
+          authed: false,
+          loading: false
+        })
+      }
+    })
+  }
+  componentWillUnmount () {
+    this.removeListener()
+  }
+  render() {
+
+      return this.state.loading === true ? <h1>Loading</h1> : (
+
+          <Switch>
+
+            <PublicRoute exact authed={this.state.autenticado} path="/login" component={Login}/>
+            <PrivateRouteEditorial authed={this.state.authed} path='/editorial' component={AdminList} />
+            <PrivateRouteEscuela authed={this.state.authed} path='/escuela' component={Escuela} />
+            <Route render={() => <h3>Uups! algo paso mal :D</h3>} />
+          </Switch>
+
+    );
+  }
 }
