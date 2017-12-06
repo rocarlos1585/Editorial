1720aec22d977b072c75619620d3dd38f47eb651
EmmanuelSkapple <emgarcia@hotmail.com>
2017-12-06 15:29:28 -0600

    Item de pedidos falta css

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('js', require('express-react-views').createEngine());
+app.set('view engine', 'js');
+
+// Static folder
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('Loginform');
+});
+
+app.post('/editorial#/Editorial/AgregarLibro', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+        user: 'emmanuelskapple@gmail.com', // generated ethereal user
+        pass: 'diosmeamara?'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer Contact" <emmanuelskapple@gmail.com>', // sender address
+      to: 'emgarcia24@hotmail.com', // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg:'Email has been sent'});
+  });
+  });
+
+app.listen(3000, () => console.log('Server started...'));
--- src/pedido.js
+++ src/pedido.js
@@ -1,6 +1,26 @@
 import React, {Component} from "react";
 import * as firebase from 'firebase';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+import Avatar from 'material-ui/Avatar';
+import MenuItem from 'material-ui/MenuItem';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 
+const style ={
+
+}
+
+const styleItem = {
+
+
+  	padding: '25px',
+  	margin: '0',
+}
+const styleImgItem ={
+  width:'30%',
+
+}
 
 
 class ItemPedido extends Component{
@@ -48,6 +68,8 @@ class ItemPedido extends Component{
       function(){
         self.obtenerImagenes(arrayLibrosActualPedido);
         self.setState({
+          NombrePedidoDB:nombreActualPedido,
+          StatusPedidoDB:statusActualPedido,
           arrayDatos:arrayLibrosActualPedido
         })
       }
@@ -58,7 +80,7 @@ class ItemPedido extends Component{
     var self=this;
     var arrayImagenes=[];
     array.forEach(function(value,index,arr){
-    var promise=new Promise(
+    var promise2=new Promise(
       function(resolve,reject){
         refLibro = firebase.database().ref('Editorial/Libros/'+value.key);
         refLibro.on('value',snapshot=>{
@@ -67,20 +89,78 @@ class ItemPedido extends Component{
         })
       }
     )
+    promise2.then(function(){
+      self.setState({
+        arrayImagenes:arrayImagenes
+      })
+
+    })
     //  console.log(value.key);
    })
-   self.setState({
-     arrayImagenes:arrayImagenes
-   })
+
+
   }
+
 
 
 
   render(){
+
     return(
-      <h1>hola</h1>
+      <div>
+      <h3>{this.state.NombrePedidoDB}</h3>
+      <h4>"Status : "{this.state.StatusPedidoDB}</h4>
+
+
+
+      <List>
+          <Items ArrayDatosLibros={this.state.arrayDatos}
+          ImagenesPedidoDB={this.state.arrayImagenes}
+          />
+      </List>
+        </div>
     );
   }
 }
+const Items = (props) =>{
+  let arrayActivo = [];
+  let arrayActivoImage = [];
+
+    for (var i = 0; i < props.ArrayDatosLibros.length; i++) {
+
+      arrayActivo[i] = props.ArrayDatosLibros[i];
+      arrayActivoImage[i]=props.ImagenesPedidoDB[i];
+
+    }
+
+
+  return(
+    <div id='items'>
+      {arrayActivo.map((mail,i) =>
+
+        <div id='admin-list-item' style={styleItem}>
+        <div>
+        <img src={arrayActivoImage[i]} style={styleImgItem}/>
+        </div>
+        <div>
+          <ListItem
+
+            primaryText= {"Titulo : "+arrayActivo[i].libro.toString()}
+            secondaryText={
+              <p>
+                {"Cantidad : "+arrayActivo[i].cantidad }
+              </p>
+
+            }
+            secondaryTextLines={2}
+          />
+          </div>
+          <Divider inset={true}/>
+        </div>
+      )}
+    </div>
+  )
+}
+
 
 export default ItemPedido;
